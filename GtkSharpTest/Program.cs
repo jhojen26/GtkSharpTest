a42aaa3a263abd22335b14f8d65ea6ba2cd8310e
jr <jr@email.com>
2019-04-02 09:49:20 -0500

    Project Creation

---
+++ GtkSharpTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Gtk;
+
+namespace GtkSharpTest
+{
+	public class GtkSharpTest
+	{
+		public static void Main()
+		{
+			Application.Init();
+
+			Window window = new Window("Hello Mono World");
+			window.Show();
+
+			Application.Run();
+		}
+	}
+}
